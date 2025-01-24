602a7252f3f140bdba356822271102a4691bc883
Sumitkumar1109 <139613858+Sumitkumar1109@users.noreply.github.com>
2025-01-24 23:45:42 +0530

    feat\landing page

--- src/pages/Auth/AuthRedirect/AuthRedirect.jsx
+++ src/pages/Auth/AuthRedirect/AuthRedirect.jsx
@@ -1,9 +1,25 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 
 function AuthRedirect() {
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      navigate('/dashboard');
+    } else {
+      navigate('/login');
+    }
+  }, [navigate]);
+
+
   return (
     <div>
-      
+      ...Loading
     </div>
   )
 }
